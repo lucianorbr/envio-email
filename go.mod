fa5f9b36a5eaf2d717fe66570002e30331b11ec8
Luciano Ribeiro <luciano.ribeiro@outlook.com>
2021-04-12 09:36:20 -0300

    Pequeno exemplo de envio de email utilizando a linguagem Golang

---
+++ go.mod
@@ -0,0 +1,1 @@
+module "envio-email"
---
+++ sendmail.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/smtp"
+)
+
+func main() {
+	// Criamos um slice do tipo string do tamanho máximo 1 para receber nosso email de destino
+	recipients := make([]string, 1)
+	recipients[0] = "email" //email de destino armazendo na posicao '0'
+
+	err := smtp.SendMail(
+		/* endereço do servidor SMTP */ "pop.gmail.com:995",
+		/* mecanismo de autenticação */ smtp.PlainAuth("", "email", "senha", "pop.gmail.com"),
+		/* email de origem */ "ribeiro.lucyanno@gmail.com",
+		/* mensagem do RFC */ recipients,
+		/* corpo do email */ []byte("Subject:Olá!\n\n Olá Luciano. Tudo de bom com Go, teste enviado!"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
